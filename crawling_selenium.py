9bf26d86db3da77a58771652173ec48a1e1204ad
dltndn <james98099@gmail.com>
2022-06-22 18:46:11 +0900

    title, href 배열 리턴 함수 작성

--- crawling_selenium.py
+++ crawling_selenium.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from time import localtime
 from bs4 import BeautifulSoup
 import requests
+import site_list
 
 class crawling_selenium:
     
@@ -12,96 +13,71 @@ class crawling_selenium:
         current_date = tm.tm_mday
         return current_date
     
-    def crawling_coindesk(self, date):
-        title_arr = []
-        href_arr = []
-        current_date = date
-        URL = "https://www.coindesk.com/"
-        driver = webdriver.Chrome()
-        driver.get(URL)
-        xpath = '//*[@id="fusion-app"]/div/div[2]/div/main/div[2]/section[1]/div/div[2]/div/div[1]/div/div/div[2]'
-        news_box = driver.find_element(By.XPATH, xpath).get_attribute("innerHTML")
-        html = BeautifulSoup(news_box, "html.parser")
-        results = html.find_all("div", {"class": "jjrrtf"})
-        
-        for result in results:
-            article_date = result.find("span", {"class": "cqrEJU"}).text
-            if "Today" in article_date or "ago" in article_date:
-                obj = result.find("a", {"class" : "gBreWF"})
-                title = obj["title"]
-                href = obj["href"]
-                title_arr.append(title)
-                href_arr.append(href)
-        
-        return title_arr, href_arr
-    
-    def crawling_cointelegraph(self, date):
-        title_arr = []
-        href_arr = []
-        current_date = date
-        URL = "https://cointelegraph.com/"
-        driver = webdriver.Chrome()
-        driver.get(URL)
-        xpath = '//*[@id="__layout"]/div/div[1]/main/div/div/div[2]/div/ul'
-        news_box = driver.find_element(By.XPATH, xpath).get_attribute("innerHTML")
-        html = BeautifulSoup(news_box, "html.parser")
-        results = html.find_all("article", {"class": "post-card__article"})
-        
-        for result in results:
-            article_date = result.find("time", {"class" : "post-card__date"}).text
-            if "hour" in article_date:
-                obj = result.find("a", {"class" : "post-card__title-link"})
-                title = obj["title"]
-                href = obj["href"]
-                title_arr.append(title)
-                href_arr.append(href)
-        
-        return title_arr, href_arr
-    
-    def crawling_bloomingbit(self, date):
-        title_arr = []
-        href_arr = []
-        current_date = date
-        URL = 'https://bloomingbit.io/news'
-        driver = webdriver.Chrome()
-        driver.get(URL)
-        xpath = '//*[@id="__layout"]/div/div[1]/div/section[2]/section[2]'
-        news_box = driver.find_element(By.XPATH, xpath).get_attribute("innerHTML")
-        html = BeautifulSoup(news_box, "html.parser")
-        results = html.find_all("a")
-        
-        for result in results:
-            title = result.find("h3").text
-            title = title.lstrip()
-            href = result["href"]
-            title_arr.append(title)
-            href_arr.append(href)
-        
-        return title_arr, href_arr
-    
-    def crawling_with_selenium(self, url, index=0, xpath=0):
+    def crawling_with_selenium(self, url, index, xpath=0):  #return arr, arr
         title_arr = []
         href_arr = []
         URL = url
         
-        if index == 0:  #selenium none
+        if index == 0:  #bitcoinmagazine
             response = requests.get(URL)
             source = response.text
             html = BeautifulSoup(source, "html.parser")
             results = html.find_all("div", {"class": "l-grid--item"})
-            
+            for result in results:
+              article_date = result.find("span" ,{"class": "mm-card--metadata-text"})
+              if article_date is not None:
+                article_date = str(result.find("span" ,{"class": "mm-card--metadata-text"}).string)
+                if "hours" in article_date:
+                  title = str(result.find("h2", {"class": "m-ellipsis--text m-card--header-text"}).string)
+                  href = result.find("a")["href"]
+                  title_arr.append(title)
+                  href_arr.append(href)
+
+            return title_arr, href_arr
+                
         else:           #selenium
             driver = webdriver.Chrome()
             driver.get(URL)
-            xpath = xpath
             source = driver.find_element(By.XPATH, xpath).get_attribute("innerHTML")
             html = BeautifulSoup(source, "html.parser")   
-            if index == 1:
+            if index == 1:  #coindesk
                 results = html.find_all("div", {"class": "jjrrtf"})
-            elif index == 2:
+                for result in results:
+                  article_date = result.find("span", {"class": "cqrEJU"}).text
+                  if "Today" in article_date or "ago" in article_date:
+                      obj = result.find("a", {"class" : "gBreWF"})
+                      title = obj["title"]
+                      href = obj["href"]
+                      title_arr.append(title)
+                      href_arr.append(href)
+              
+                return title_arr, href_arr
+              
+            elif index == 2:  #cointelegraph
                 results = html.find_all("article", {"class": "post-card__article"})
-            elif index == 3:
-                results = html.find_all("a")        
+                for result in results:
+                  article_date = result.find("time", {"class" : "post-card__date"}).text
+                  if "hour" in article_date:
+                      obj = result.find("a", {"class" : "post-card__title-link"})
+                      title = obj["title"]
+                      href = obj["href"]
+                      title_arr.append(title)
+                      href_arr.append(href)
+              
+                return title_arr, href_arr
+              
+            elif index == 3:  #bloomingbit
+                results = html.find_all("a")
+                for result in results:
+                  title = result.find("h3").text
+                  title = title.lstrip()
+                  href = result["href"]
+                  title_arr.append(title)
+                  href_arr.append(href)
+              
+                return title_arr, href_arr    
         
 test = crawling_selenium()
-test.crawling_coindesk(test.get_current_date())
+sl = site_list.site_list()
+
+
--- site_list.py
+++ site_list.py
@@ -1,13 +1,16 @@
-
-site_list = {
-  'bitcoinmagazine' : {"url" : 'https://bitcoinmagazine.com/',
-                       "index" : 0},
-  'coindesk' : {"url" : 'https://www.coindesk.com/',
-                "index" : 1},
-  'cointelegraph' : {"url" : 'https://cointelegraph.com/',
-                     "index" : 2},
-  'bloomingbit' : {"url" : 'https://bloomingbit.io/news',
-                   "index" : 3}
-}
-
-print(site_list['bitcoinmagazine']['index'])
+class sl:
+      
+  site_list = {
+    'bitcoinmagazine' : {"url" : 'https://bitcoinmagazine.com/',
+                        "index" : 0},
+    'coindesk' : {"url" : 'https://www.coindesk.com/',
+                  "index" : 1},
+    'cointelegraph' : {"url" : 'https://cointelegraph.com/',
+                      "index" : 2},
+    'bloomingbit' : {"url" : 'https://bloomingbit.io/news',
+                    "index" : 3}
+  }
+  
+  coindesk_xpath = '//*[@id="fusion-app"]/div/div[2]/div/main/div[2]/section[1]/div/div[2]/div/div[1]/div/div/div[2]'
+  cointelegraph_xpath = '//*[@id="__layout"]/div/div[1]/main/div/div/div[2]/div/ul'
+  blommingbit_xpath = '//*[@id="__layout"]/div/div[1]/div/section[2]/section[2]'
